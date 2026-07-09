c1caba83c78db0cba7c09f977e59d7f2570e3e71
ernanhughes <155574808+ernanhughes@users.noreply.github.com>
2026-07-09 11:20:34 +0100

    Add preference futures package

---
+++ src/preference_futures/__init__.py
@@ -0,0 +1,5 @@
+"""Preference Futures research package."""
+
+from preference_futures.episodes import PreferenceEpisode, RevisionTriplet, build_preference_episode
+
+__all__ = ["PreferenceEpisode", "RevisionTriplet", "build_preference_episode"]
